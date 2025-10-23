c5904fcf25342a5c698117ee1042eb20045f9fdb
Eanicy <edwin.allanicjr.23@gmail.com>
2025-10-23 14:30:19 +0800

    Add few line

---
+++ printing-project/resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<?php
+//test request
+$requests = [
+    [
+        'id' => 1,
+        'name' => 'John Doe',
+        'request' => 'Reset password',
+        'status' => 'Pending'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Jane Smith',
+        'request' => 'Update email address',
+        'status' => 'Completed'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Mark Johnson',
+        'request' => 'Request account statement',
+        'status' => 'In Progress'
+    ]
+];
+?>
