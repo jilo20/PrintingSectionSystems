c8d299bf67e0034dcfbb8b774e377549144757bd
Josh C. Rostata <joshrostata290@gmail.com>
2025-10-25 23:46:56 +0800

    added a few sample requests

--- printing-project/resources/views/welcome.blade.php
+++ printing-project/resources/views/welcome.blade.php
@@ -19,5 +19,29 @@ $requests = [
         'request' => 'Request account statement',
         'status' => 'In Progress'
     ]
+    [
+        'id' => 4,
+        'name' => 'Jonah Jameson',
+        'request' => 'Request color printing',
+        'status' => 'In Progress'
+    ]
+    [
+        'id' => 5,
+        'name' => 'Darren Villanueva',
+        'request' => 'Request reprint of documents',
+        'status' => 'Pending'
+    ]
+    [
+        'id' => 6,
+        'name' => 'Alicia Santos',
+        'request' => 'Cancel previously submitted print request',
+        'status' => 'Cancelled'
+    ]
+    [
+        'id' => 7,
+        'name' => 'Ella Mendoza',
+        'request' => 'Print activity sheets',
+        'status' => 'Pending'
+    ]
 ];
 ?>
