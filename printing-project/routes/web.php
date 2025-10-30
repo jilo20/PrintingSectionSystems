67db6e13489846bf726de8146661451ff583375a
jilo20 <angelobazar20@gmail.com>
2025-10-30 11:26:10 +0800

    Added Something

--- printing-project/routes/web.php
+++ printing-project/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 });
 
 Route::get('/login', [AuthController::class,'showLogin'])->name('show.login');
