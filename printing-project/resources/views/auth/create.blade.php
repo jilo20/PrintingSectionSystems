abbf3167c850005acb1221858556032926b1f5f7
Josh C. Rostata <joshrostata290@gmail.com>
2025-11-03 18:24:35 +0800

    added a temporary file for the create job window

---
+++ printing-project/resources/views/auth/create.blade.php
@@ -0,0 +1,9 @@
+<!-- Temporary placement -->
+
+
+<!-- Name of Requestor -->
+<!-- Department Name -->
+<!-- Job Name -->
+<!-- Number of copies to be printed -->
+<!-- Type of file -->
+<!-- File name -->
